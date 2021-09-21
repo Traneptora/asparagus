5ab5ac25775052681b53361db5fe6e8eb26ef41f
Leo Izen <leo.izen@gmail.com>
2021-09-20 23:34:37 -0400

    use python requests to do the requests

--- asparagus.py
+++ asparagus.py
@@ -3,9 +3,9 @@
 import asyncio, signal
 import os, re, sys, time
 import traceback
-import urllib.request
 
 import discord
+import requests
 from discord.ext import tasks
 
 from deepbluesky import DeepBlueSky
@@ -34,17 +34,16 @@ async def retrieve_rss():
 
 async def retrieve_latest(cache_filename, url, regex, new_page_prefix, ping_role):
     remote_version = None
-    request = urllib.request.Request(url, headers={'User-Agent': 'curl/7.76.1'})
     try:
-        with urllib.request.urlopen(request, timeout=5) as connection:
-            while line := connection.readline():
-                match = regex.search(line.decode('UTF-8'))
-                if match:
-                    remote_version = match.group(1)
-                    break
-    except BaseException as error:
+        response = requests.get(url, headers={'User-Agent': 'curl/7.76.1'}, timeout=5)
+        for line in response.text.split('\n'):
+            match = regex.search(line)
+            if match:
+                remote_version = match.group(1)
+                break
+    except Exception as error:
         client.logger.exception('Unable to obtain remote version')
-    if remote_version == None:
+    if remote_version is None:
         client.logger.error(f'Error retreieving latest remote: {url}')
         return
 
@@ -53,6 +52,7 @@ async def retrieve_latest(cache_filename, url, regex, new_page_prefix, ping_role
             cached_version = cache_file.read()
     except IOError:
         cached_version = ''
+
     if remote_version != cached_version:
         client.logger.info(f'Found update: {cache_filename}')
         channel_id = TBWF_ANNOUNCE_CHANNEL_ID
@@ -71,7 +71,7 @@ async def retrieve_latest(cache_filename, url, regex, new_page_prefix, ping_role
 @client.event
 async def on_ready():
     client.logger.info(f'Logged in as {client.user}')
-    game = discord.Game("I'm an Asparagus")
+    game = discord.Game('I’m an Asparagus')
     await client.change_presence(status=discord.Status.online, activity=game)
     retrieve_rss.start()
 
